9722f1f1e892f2b12f4405e383e1f78213334333
Josh Pollock <josh@calderawp.com>
2019-01-28 16:01:23 -0500

    require babel plugins

---
+++ boilerplate-plugin-with-blocks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+
+ * Plugin Name: WordPress Plugin
+ * Description:
+ * Version:     0.1.0
+ * Author:      CalderaWP
+ * Author URI:  https://CalderaWP.com
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// Enqueue editor UI scripts & styles.
+require_once( __DIR__ . '/inc/asset-loader.php' );
+require_once( __DIR__ . '/inc/scripts.php' );
+HMR_Demo\Scripts\setup();
